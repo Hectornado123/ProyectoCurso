2957b4a1dfd32b69b09533dea5c124c071fac025
Isakeitor <isaacrubioalfonso@gmail.com>
2026-02-03 10:41:45 +0100

    CanvasPrueba

---
+++ PI2D_Project/Assets/_PI2D_Root/Scripts/ContenedorCorazones.cs
@@ -0,0 +1,39 @@
+using System;
+using UnityEngine;
+
+public class ContenedorCorazones : MonoBehaviour
+{
+    [SerializeField] private CorazonUI[] corazones;
+    private VidaJugador vidaJugador;
+
+    private void Start()
+    {
+        vidaJugador = FindFirstObjectByType<VidaJugador>();
+
+        vidaJugador.JugadorTomoDaño += ActivarCorazones;
+        vidaJugador.JugadorSeCuro += ActivarCorazones;
+
+        ActivarCorazones(vidaJugador.GetVidaActual());
+    }
+
+    void OnDisable()
+    {
+        vidaJugador.JugadorTomoDaño -= ActivarCorazones;
+        vidaJugador.JugadorSeCuro -= ActivarCorazones;
+    }
+
+    private void ActivarCorazones(int vidaActual)
+    {
+        for (int i = 0; i < corazones.Length; i++)
+        {
+            if (i < vidaActual)
+            {
+                corazones[i].ActivarCorazon();
+            }
+            else
+            {
+                corazones[i].DesactivarCorazon();
+            }
+        }
+    }
+}
