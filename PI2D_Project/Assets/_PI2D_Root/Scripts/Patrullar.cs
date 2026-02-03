58881086797e0047d2d68e10acb5dd7609f5bc9c
Isakeitor <isaacrubioalfonso@gmail.com>
2026-02-03 12:37:31 +0100

    Patrullar/Vida scripts

---
+++ PI2D_Project/Assets/_PI2D_Root/Scripts/Patrullar.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class Patrullar : MonoBehaviour
+{
+
+    [SerializeField] private float velocidadMovimiento;
+    [SerializeField] private Transform[] puntosMovimiento;
+    [SerializeField] private float distanciaMinima;
+
+    private int numeroAleatorio;
+    private SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        numeroAleatorio = Random.Range(0, puntosMovimiento.Length);
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        Girar();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, puntosMovimiento[numeroAleatorio].position, velocidadMovimiento * Time.deltaTime);
+
+        if (Vector2.Distance(transform.position, puntosMovimiento[numeroAleatorio].position) < distanciaMinima)
+        {
+            numeroAleatorio = Random.Range(0, puntosMovimiento.Length);
+            Girar();
+        }
+    }
+
+    private void Girar()
+    {
+        if (transform.position.x < puntosMovimiento[numeroAleatorio].position.x)
+        {
+            spriteRenderer.flipX = true;
+        }
+        else
+        {
+            spriteRenderer.flipX = false;
+        }
+    }
+}
--- PI2D_Project/Assets/_PI2D_Root/Scripts/PlayerController.cs
+++ PI2D_Project/Assets/_PI2D_Root/Scripts/PlayerController.cs
@@ -1,8 +1,6 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 using System.Collections;
-using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
