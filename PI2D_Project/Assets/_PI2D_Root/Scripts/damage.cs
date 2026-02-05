177ace05daf070f18399ba3fd1a119b39c824f6b
Dakasta <diegpsanrracho@gmail.com>
2026-02-05 07:02:37 +0100

    scriptdamage

---
+++ PI2D_Project/Assets/_PI2D_Root/Scripts/damage.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int vidaMaxima = 3;
+    public int vidaActual;
+
+    void Start()
+    {
+        vidaActual = vidaMaxima;
+        Debug.Log("Vida inicial: " + vidaActual);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("enemigo"))
+        {
+            QuitarVida(1);
+        }
+
+        if (other.CompareTag("filo"))
+        {
+            QuitarVida(3);
+        }
+    }
+
+    void QuitarVida(int cantidad)
+    {
+        vidaActual -= cantidad;
+        Debug.Log("Daño recibido: " + cantidad + " | Vida: " + vidaActual);
+
+        if (vidaActual <= 0)
+        {
+            Morir();
+        }
+    }
+
+    void Morir()
+    {
+        Debug.Log("💀 PLAYER MUERTO");
+        // Aquí puedes:
+        // Destroy(gameObject);
+        // Recargar escena
+        // Animación de muerte
+    }
+}
