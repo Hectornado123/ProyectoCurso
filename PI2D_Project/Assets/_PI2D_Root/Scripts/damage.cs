0285f80725eec121f576d88e0b977517b8eb311e
Hectornado123 <Hecalbgon1@gmail.com>
2026-02-05 21:51:29 +0100

    aaa

--- PI2D_Project/Assets/_PI2D_Root/Scripts/damage.cs
+++ PI2D_Project/Assets/_PI2D_Root/Scripts/damage.cs
@@ -1,48 +1,56 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour
 {
     public int vidaMaxima = 3;
     public int vidaActual = 3;
-
+    private Respawn Respawn;
     void Start()
     {
         vidaActual = vidaMaxima;
+        Respawn = GetComponent<Respawn>();
         Debug.Log("Vida inicial: " + vidaActual);
     }
 
-    void OnTriggerEnter2D(Collider2D other)
+    void OnCollisionEnter2D(Collision2D collision)
     {
-        if (other.CompareTag("enemigo"))
+        if (collision.gameObject.CompareTag("enemigo"))
         {
             vidaActual--;
-            
-            
-            //QuitarVida(1);
         }
 
-        if (other.CompareTag("filo"))
-        {
-            vidaActual = 0;
-        }
+      
 
         if (vidaActual <= 0)
         {
             Morir();
         }
     }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("filo"))
+        {
+            Morir();
+        }
+    }
+    // void QuitarVida(int cantidad)
+    // {
+    //      vidaActual -= cantidad;
+    //  Debug.Log("Daño recibido: " + cantidad + " | Vida: " + vidaActual);
 
-   // void QuitarVida(int cantidad)
-   // {
-  //      vidaActual -= cantidad;
-      //  Debug.Log("Daño recibido: " + cantidad + " | Vida: " + vidaActual);
 
-      
-   // }
+    // }
 
     void Morir()
     {
         Debug.Log("💀 PLAYER MUERTO");
+        // Respawn
+        Respawn.Respaawn();
+
+        // Resetear vida
+        vidaActual = vidaMaxima;
         // Aquí puedes:
         // Destroy(gameObject);
         // Recargar escena
